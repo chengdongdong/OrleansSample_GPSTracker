3ba75096b962597609aedf171a980b4ba653ddb4
wuzhenda <wu.publicmail@gmail.com>
2019-08-16 16:35:53 +0800

    init

---
+++ GPSTracker.Web/Controllers/LocationHub.cs
@@ -0,0 +1,37 @@
+﻿using GPSTracker.Common;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Controllers
+{
+    public class LocationHub : Hub
+    {
+        public void LocationUpdate(VelocityMessage message)
+        {
+            // Forward a single messages to all browsers
+            Clients.Group("BROWSERS").locationUpdate(message);
+        }
+
+        public void LocationUpdates(VelocityBatch messages)
+        {
+            // Forward a batch of messages to all browsers
+            Clients.Group("BROWSERS").locationUpdates(messages);
+        }
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            if (Context.Headers.Get("ORLEANS") != "GRAIN")
+            {
+                // This connection does not have the GRAIN header, so it must be a browser
+                // Therefore add this connection to the browser group
+                Groups.Add(Context.ConnectionId, "BROWSERS");
+            }
+            return base.OnConnected();
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Orleans Samples GPS Tracker
+## original url
+https://github.com/dotnet/orleans/tree/master/Samples/1.x/GPSTracker
+fixed some error
+
+# TODO,update orleans lib
